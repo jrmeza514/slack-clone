7290f7c528309e0df32bc766f35ba5b5d5808986
Juan Meza <jrmeza514@gmail.com>
2016-05-22 16:58:15 -0700

    Basic add message

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -2,34 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 var messages = [
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Juan Meza', body: 'This is a message'},
-  { author: 'John Doe', body: 'This is another message'},
-  { author: 'Jane Doe', body: 'This is a better message'}
+
 ];
 
 router.route('/')
 .get(function( req, res ){
   res.json(messages);
+});
+
+router.route('/add')
+.get(function( req, res ){
+  var name = req.query.name;
+  var body = req.query.body;
+
+  if ( name && body ) {
+    console.log("successfull reques");
+    res.status( 200 );
+    req.redirect('/');
+    message.push({
+      author: name,
+      body: body
+    });
+
+  }else{
+    res.status( 400 );
+    res.send("Bad Request: Check Parameters");
+  }
 });
 
 
