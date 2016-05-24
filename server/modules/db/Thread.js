fbcd3f8d2080a421efb14d7e40993a73b70372f8
Juan Meza <jrmeza514@gmail.com>
2016-05-24 00:03:23 -0700

    Ensure User exists before adding to the thread

--- server/modules/db/Thread.js
+++ server/modules/db/Thread.js
@@ -1,5 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
+const UsersManager = require('./UsersManager.js');
 
+const THREADS_COLLECTION = "threads";
+const MONGODB_URL = "mongodb://localhost:27017/slack-clone";
 
 class Thread {
   constructor( thread ) {
@@ -19,8 +22,42 @@ class Thread {
     return this.members;
   }
 
+  // TODO: This thread add a even if he does not exist
+  // Chech user before adding to the thread
   addMember( memberId ){
-  
+    let self = this;
+    return new Promise(( resolve, reject ) => {
+      let usersManager = new UsersManager();
+      usersManager.findUser( memberId )
+      /* User Found */
+      .then( user => {
+        MongoClient.connect( MONGODB_URL )
+        /* Database Connection successful */
+        .then( db => {
+          let threadsCollection = db.collection( THREADS_COLLECTION );
+
+          threadsCollection.update(
+            { threadId: self.getThreadId() },
+            { $push: { members : memberId }}
+          )
+          .then( result => {
+            db.close();
+            resolve();
+          })
+          .catch( err => {
+            db.close();
+            reject( new Error('User Already Exists') );
+          });
+        })
+        /* Database Connection Failed */
+        .catch( err => {
+          reject( err );
+        });
+      })
+      .catch( err => {
+        reject("User Does Not Exist");
+      });
+    });
   }
 
   addMessage( message ){
