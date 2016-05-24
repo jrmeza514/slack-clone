c91ad10cdaae11e05638484d0bf802a77e6985c2
Juan Meza <jrmeza514@gmail.com>
2016-05-24 12:59:13 -0700

    Formatted Comments

--- server/modules/db/Thread.js
+++ server/modules/db/Thread.js
@@ -1,68 +1,88 @@
 const MongoClient = require('mongodb').MongoClient;
+const User = require('./User.js');
 const UsersManager = require('./UsersManager.js');
 
 const THREADS_COLLECTION = "threads";
 const MONGODB_URL = "mongodb://localhost:27017/slack-clone";
 
 class Thread {
-  constructor( thread ) {
-    this.threadId = thread.threadId;
-    this.messages = thread.messages;
-    this.members = thread.members;
-  }
-  getThreadId(){
-    return this.threadId;
-  }
+	constructor( thread ) {
+		this.threadId = thread.threadId;
+		this.messages = thread.messages;
+		this.members = thread.members;
+	}
 
-  getMessages(){
-    return this.messages;
-  }
+	getThreadId(){
+		return this.threadId;
+	}
 
-  getMembers(){
-    return this.members;
-  }
+	getMessages(){
+		return this.messages;
+	}
 
-  // TODO: This thread add a even if he does not exist
-  // Chech user before adding to the thread
-  addMember( memberId ){
-    let self = this;
-    return new Promise(( resolve, reject ) => {
-      let usersManager = new UsersManager();
-      usersManager.findUser( memberId )
-      /* User Found */
-      .then( user => {
-        MongoClient.connect( MONGODB_URL )
-        /* Database Connection successful */
-        .then( db => {
-          let threadsCollection = db.collection( THREADS_COLLECTION );
+	getMembers(){
+		return this.members;
+	}
 
-          threadsCollection.update(
-            { threadId: self.getThreadId() },
-            { $push: { members : memberId }}
-          )
-          .then( result => {
-            db.close();
-            resolve();
-          })
-          .catch( err => {
-            db.close();
-            reject( new Error('User Already Exists') );
-          });
-        })
-        /* Database Connection Failed */
-        .catch( err => {
-          reject( err );
-        });
-      })
-      .catch( err => {
-        reject("User Does Not Exist");
-      });
-    });
-  }
+	/*
+		This method should return a promise that will either:
+			- resolve if the user is Successfully added to the Thread
+			- reject if the usser Fails to join the thread
 
-  addMessage( message ){
+		Requirements: User must already Exist
+	*/
+	addMember( memberId ){
+		let self = this;
+		return new Promise(( resolve, reject ) => {
+			let usersManager = new UsersManager();
+			/* Search if user Exists */
+			usersManager.findUser( memberId )
+			/* User Found */
+			.then( user => {
+				MongoClient.connect( MONGODB_URL )
+				/* Database Connection successful */
+				.then( db => {
+					let threadsCollection = db.collection( THREADS_COLLECTION );
+					/* Update the Thread's members field to push a new member */
+					threadsCollection.update(
+						{ threadId: self.getThreadId() },
+						{ $push: { members : memberId }}
+					)
+					/* Successfully Updated Thread */
+					.then( result => {
+						db.close();
+						resolve();
+					})
+					/* Failed To Update Thread:  */
+					.catch( err => {
+						db.close();
+						reject( new Error("User Already Exists") );
+					});
+				})
+				/* Database Connection Failed */
+				.catch( err => {
+					reject( new Error("Database Connection Failed") );
+				});
+			})
+			/* User Not Found */
+			.catch( err => {
+				reject( new Error("User Does Not Exist") );
+			});
+		});
+	}
 
-  }
+	/*
+		This method should return a Promise that will:
+			- resolve: if the message is Successfully added
+			- reject: if the message is not Successfully Added
+
+		Requirements: 
+	*/
+	addMessage( message ){
+		return new Promise((resolve, reject) => {
+
+		});
+	}
 }
 
 module.exports = Thread;
--- server/modules/db/ThreadsManager.js
+++ server/modules/db/ThreadsManager.js
@@ -6,107 +6,87 @@ const THREADS_COLLECTION = "threads";
 const MONGODB_URL = "mongodb://localhost:27017/slack-clone";
 
 class ThreadsManager {
-  constructor(){
+	constructor(){}
+	createThread( threadId ){
+		return new Promise(( resolve, reject ) => {
+			MongoClient.connect( MONGODB_URL )
+			/* Database Connection successful */
+			.then(( db ) => {
+				/* Select The Threads collection */
+				let threadsCollection = db.collection(THREADS_COLLECTION);
+				/* Attempt to insert the new thread. This will return a Promise */
+				threadsCollection.insert({
+					"threadId": threadId,
+					"messages": [],
+					"members": []
+				})
+				/* Insertion successful */
+				.then( result => {
+					resolve( new Thread( result.ops[0]) );
+					db.close();
+				})
+				/* Insertion Failed: Close db and reject Promise */
+				.catch( err => {
+					db.close();
+					reject( err );
+				});
+			})
+			/* Database Connection Failed: reject the promise */
+			.catch( err => {
+				reject( err );
+			});
+		});
+	}
 
-  }
+	deleteThread( threadId ){
+		return new Promise(( resolve, reject ) => {
+			MongoClient.connect( MONGODB_URL )
+			/* Database Connection successful */
+			.then( db => {
+				let threadsCollection = db.collection( THREADS_COLLECTION );
+				threadsCollection.remove({"threadId": threadId})
+				/* Thread Deletion successful */
+				.then( () => {
+					db.close();
+					resolve();
+				})
+				/* Thread Deletion Failed  */
+				.catch( err => {
+					db.close();
+					reject( err );
+				})
+			})
+			/* Database connection failed */
+			.catch( err => {
+				reject( err );
+			});
+		});
+	}
 
-  createThread( threadId ){
-    return new Promise(( resolve, reject ) => {
-      /*  Connect to the Database with the Url Provided */
-      MongoClient.connect( MONGODB_URL )
-
-      /* Database Connection successful */
-      .then(( db ) => {
-
-        /* Select The Threads collection */
-        let threadsCollection = db.collection(THREADS_COLLECTION);
-
-        /* Attempt to insert the new thread. This will return a Promise */
-        threadsCollection.insert({
-          "threadId": threadId,
-          "messages": [],
-          "members": []
-        })
-        /*
-          Handle a successful insertion by resolving the Promise
-          and Closing the Database connection
-        */
-        .then( result => {
-          resolve( new Thread( result.ops[0]) );
-          db.close();
-        })
-        /* Insertion Failed: Close db and reject Promise */
-        .catch( err => {
-          db.close();
-          reject( err );
-        });
-      })
-      /* Database Connection Failed: reject the promise */
-      .catch( err => {
-        reject( err );
-      });
-    });
-  }
-
-  deleteThread( threadId ){
-
-    return new Promise(( resolve, reject ) => {
-
-      MongoClient.connect( MONGODB_URL )
-      /* Database Connection successful */
-      .then( db => {
-        let threadsCollection = db.collection( THREADS_COLLECTION );
-
-        threadsCollection.remove({"threadId": threadId})
-
-        /* Thread Deletion successful */
-        .then( () => {
-          db.close();
-          resolve();
-        })
-
-        /* Thread Deletion Failed  */
-        .catch( err => {
-          db.close();
-          reject( err );
-        })
-      })
-
-      /* Database connection failed */
-      .catch( err => {
-        reject( err );
-      });
-
-    });
-  }
-
-  findThread( threadId ){
-    return new Promise(( resolve, reject ) => {
-      MongoClient.connect( MONGODB_URL )
-      /* Database Connection successful */
-      .then( db => {
-        let threadsCollection = db.collection( THREADS_COLLECTION );
-
-        threadsCollection.find({"threadId": threadId}).toArray()
-
-        /* Found Thread */
-        .then( thread => {
-          db.close();
-          resolve( new Thread( thread[0] ) );
-        })
-        /* Found Nothing */
-        .catch( err => {
-          db.close();
-          reject( err );
-        })
-      })
-      /* Database Connection Failed */
-      .catch( err => {
-        reject( err );
-      });
-
-    });
-  }
+	findThread( threadId ){
+		return new Promise(( resolve, reject ) => {
+			MongoClient.connect( MONGODB_URL )
+			/* Database Connection successful */
+			.then( db => {
+				let threadsCollection = db.collection( THREADS_COLLECTION );
+				threadsCollection.find({"threadId": threadId}).toArray()
+				/* Found Thread */
+				.then( thread => {
+					db.close();
+					resolve( new Thread( thread[0] ) );
+				})
+				/* Found Nothing */
+				.catch( err => {
+					db.close();
+					reject( err );
+				})
+			})
+			/* Database Connection Failed */
+			.catch( err => {
+				reject( err );
+			});
+		});
+	}
 }
 
 module.exports = ThreadsManager;
