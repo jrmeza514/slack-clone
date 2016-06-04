04a2707ab98e9bf9e5533f7f9357905a359e0619
Juan Meza <jrmeza514@gmail.com>
2016-06-04 01:02:30 -0700

    feat: Session Management
    The user class can now retrieve all of its active sessions from the
    database and add a new session to it.

--- server/modules/db/User.js
+++ server/modules/db/User.js
@@ -1,3 +1,7 @@
+const MongoClient = require('mongodb').MongoClient;
+const USERS_COLLECTION = "users";
+const MONGODB_URL = "mongodb://localhost:27017/slack-clone";
+
 class User{
   constructor( user ){
     this.passwordHash = user.passwordHash;
@@ -16,6 +20,60 @@ class User{
   getEmail(){
     return this.email;
   }
+
+  getActiveUserSessions(){
+		let self = this;
+		return new Promise(( resolve , reject ) => {
+			MongoClient.connect( MONGODB_URL )
+			.then( db => {
+				let usersCollection = db.collection( USERS_COLLECTION );
+
+				usersCollection.find(
+					{ 'userId' : self.getUserId() },
+					{ 'sessions' : true }
+				).toArray()
+				.then(( data ) => {
+					db.close();
+					resolve( data[0].sessions );
+				})
+				.catch((err) => {
+					db.close();
+					reject(new Error('No Users Found'));
+				});
+			})
+			.catch((err) => {
+				reject( new Error('Database Connection Failed') );
+			});
+
+		});
+  }
+
+	addActiveSession( session ){
+		let self = this;
+		return new Promise(( resolve, reject ) => {
+			MongoClient.connect( MONGODB_URL )
+			.then(( db ) => {
+				let usersCollection = db.collection( USERS_COLLECTION );
+
+				usersCollection.update(
+					{ userId: self.getUserId() },
+					{ $push: { sessions: session } }
+				)
+
+				.then( results => {
+					db.close();
+					resolve();
+				})
+				.catch((err) => {
+					db.close();
+					reject(new Error('Unable To Create Session'));
+				});
+			})
+			.catch((err) => {
+				reject( new Error('Database Connection Failed'));
+			});
+		});
+	}
 }
 
 module.exports = User;
