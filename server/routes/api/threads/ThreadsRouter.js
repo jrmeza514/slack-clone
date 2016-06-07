bec9df1c47de856d70176c97cb9568a93de3c3eb
Juan Meza <jrmeza514@gmail.com>
2016-06-06 19:29:00 -0700

    Threads ID Router
    Moved All ID logic into its own router.

--- server/routes/api/threads/ThreadsRouter.js
+++ server/routes/api/threads/ThreadsRouter.js
@@ -12,44 +12,25 @@ const threadsManager = new ThreadsManager();
 const authManager = new AuthManager();
 
 const router = express.Router();
-const IDRouter = express.Router({ mergeParams: true });
+const IDRouter = require('./IDRouter/IDRouter.js');
 
-router.use('/:threadId', IDRouter );
+router.use('/:threadId', IDRouter);
 
 /* Get All Threads */
 router.route('/')
-.get(( req, res ) => {
-	threadsManager.getAllThreads()
-	.then( threads => {
-		res.json({
-			results: threads
-		});
-	})
-	.catch( err => {
-		res.json({
-			result: null,
-			message: `No Threads Found.`
-		});
-	})
-});
-
-/* GET THREAD BY ID */
-IDRouter.route('/')
-.get(( req, res ) => {
-	let threadId = req.params.threadId;
-	threadsManager.findThread( threadId )
-	.then( thread => {
-		res.json({
-			results: thread
-		});
-	})
-	.catch( err => {
-		res.json({
-			results: null,
-			message: `Nothing found for ${threadId}`
-		});
+	.get((req, res) => {
+		threadsManager.getAllThreads()
+			.then(threads => {
+				res.json({
+					results: threads
+				});
+			})
+			.catch(err => {
+				res.json({
+					result: null,
+					message: `No Threads Found.`
+				});
+			})
 	});
-});
-
 
 module.exports = router;
